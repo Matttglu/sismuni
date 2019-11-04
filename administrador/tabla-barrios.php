ce3cd8903ebb964948cdb6be51b3ad34a3337acb
leandrovalen <weberleandro1984@gmail.com>
2019-11-04 14:08:57 -0300

    modificaciones en el nav

--- administrador/tabla-barrios.php
+++ administrador/tabla-barrios.php
@@ -61,12 +61,12 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-folder"></i>
-          <span>Páginas</span>
+          <span>Herramientas</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="register.html">Registrarse</a>
+            <h6 class="collapse-header">Cuenta:</h6>
+            <a class="collapse-item" href="register.html">Registrar</a>
           </div>
         </div>
       </li>
